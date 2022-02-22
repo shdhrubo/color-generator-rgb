24337282ebfde39024dd169601a096b52037c496
shdhrubo <shorifulhabib.iit@gmail.com>
2022-02-22 11:57:03 +0600

    color gererator

---
+++ script.js
@@ -0,0 +1,14 @@
+//function for rgb color generating
+function RGB(){
+const x=Math.floor(Math.random()*255);
+const y=Math.floor(Math.random()*255);
+const z=Math.floor(Math.random()*255);
+return `rgb(${x},${y},${z})`;
+}
+//event handelar in button
+document.getElementById("btn").addEventListener('click',function(){
+    const bgColor=RGB();
+    document.body.style.backgroundColor=bgColor;
+    document.getElementById("result").innerText=bgColor;
+
+})
