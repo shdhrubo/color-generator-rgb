d27d088bbbaae8e597987d12ef13b610b9a2c0aa
shdhrubo <shorifulhabib.iit@gmail.com>
2022-03-19 17:56:00 +0600

    new deploy

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const x=Math.floor(Math.random()*255);
 const y=Math.floor(Math.random()*255);
 const z=Math.floor(Math.random()*255);
 return `rgb(${x},${y},${z})`;
+// return 'rgb('+x+','+y+','+z+')';
 }
 //event handelar in button
 document.getElementById("btn").addEventListener('click',function(){
     const bgColor=RGB();
     document.body.style.backgroundColor=bgColor;
     document.getElementById("result").innerText=bgColor;
+
 
 })
